2243c89a58912993e7c79a998ebd7a06047693d0
redzer0 <redzer0@yandex.ru>
2020-08-19 09:27:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict'; 
+a=5;
+console.log(a);
+
+let persone={
+    name:"John",
+    age: 25,
+    isMarried:false
+};
+console.log(persone.name);
+let arr=['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[1]);
+/* alert('Helo world!'); */
+/* let anwser=confirm("are you here?");
+console.log(anwser); */
+/*  let anwser = +prompt("do you 18?", "Yes"); 
+console.log(typeof(anwser) ); */
+/* console.log("arr"+" - object");
+console.log(4 + " - object"); */
+let incr=10,
+    decr=10;
+/* incr++;
+decr--; */
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2"==2);
+
+let isChecked=true,
+isClose=false;
+console.log(isChecked && isClose);
+
