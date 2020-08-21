e07860bcb487b86d0a6eaf56836b6009f44fd635
redzer0 <redzer0@yandex.ru>
2020-08-21 09:28:38 +0300

    some text

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
         b = prompt("Во сколько обойдется?", "");
 
-    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {  
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && (a.length) < 50) {  
         console.log('done');
         appData.expenses[a] = b;
     } else{   
